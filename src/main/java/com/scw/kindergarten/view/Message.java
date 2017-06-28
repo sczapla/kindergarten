243bb6bd3d2e59afefcbe6a1e94e5ff03ed3d7cc
sczapla <sylwia.czapla@interia.pl>
2017-06-28 13:39:48 +0200

    widok wiadomosci i platnosci

--- src/main/java/com/scw/kindergarten/view/Message.java
+++ src/main/java/com/scw/kindergarten/view/Message.java
@@ -1,38 +1,63 @@
 package com.scw.kindergarten.view;
 
 public class Message {
+	private Integer id;
 	private String from;
 	private String topic;
 	private String date;
-	
-	
+	private String content;
+
 	public Message() {
 		super();
 	}
-	public Message(String from, String topic, String date) {
+
+	public Message(Integer id, String from, String topic, String date, String content) {
 		super();
+		this.id = id;
 		this.from = from;
 		this.topic = topic;
 		this.date = date;
+		this.content = content;
 	}
+
 	public String getFrom() {
 		return from;
 	}
+
 	public void setFrom(String from) {
 		this.from = from;
 	}
+
 	public String getTopic() {
 		return topic;
 	}
+
 	public void setTopic(String topic) {
 		this.topic = topic;
 	}
+
 	public String getDate() {
 		return date;
 	}
+
 	public void setDate(String date) {
 		this.date = date;
 	}
-	
-	
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
 }
--- src/main/java/com/scw/kindergarten/view/MessageView.java
+++ src/main/java/com/scw/kindergarten/view/MessageView.java
@@ -2,34 +2,44 @@ package com.scw.kindergarten.view;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
-import javax.faces.bean.ViewScoped;
+import javax.faces.application.FacesMessage;
 
-import org.primefaces.model.timeline.TimelineEvent;
-import org.primefaces.model.timeline.TimelineModel;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import com.scw.kindergarten.view.utils.Utils;
+
 @Component("messageView")
-@ViewScoped
-public class MessageView {
-	private List<Message> messages;  
- 
-    @PostConstruct
-    public void init() {
-        createFirstTimeline();
-    }
- 
-    private void createFirstTimeline() {
-    	messages = new ArrayList<Message>();
-    	messages.add(new Message("Dyrektor", "Zebranie", "11/03/2017"));
-    	messages.add(new Message("Nauczyciel", "Obuwie", "12/03/2017"));
-    	messages.add(new Message("Nauczyciel", "Wycieczka", "13/03/2017"));
-    	messages.add(new Message("Nauczyciel", "Szczotka do wymiany", "14/03/2017"));
-    }
+@Scope("view")
+public class MessageView implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private List<Message> messages;
+	private List<Message> selectedMsg;
+	private List<Message> filteredValue;
+	private Integer id;
+
+	@PostConstruct
+	public void init() {
+		createFirstTimeline();
+	}
+
+	private void createFirstTimeline() {
+		messages = new ArrayList<Message>();
+		messages.add(new Message(1, "Dyrektor", "Zebranie", "11/03/2017", "przykladowa tresc"));
+		messages.add(new Message(2, "Nauczyciel", "Obuwie", "12/03/2017", "przykladowa tresc2"));
+		messages.add(new Message(3, "Nauczyciel", "Wycieczka", "13/03/2017", "przykladowa tresc3"));
+		messages.add(new Message(4, "Nauczyciel", "Szczotka do wymiany", "14/03/2017", "przykladowa tresc4"));
+		messages.add(new Message(5, "Nauczyciel", "Szczotka do wymiany", "14/03/2017", "przykladowa tresc4"));
+		messages.add(new Message(6, "Nauczyciel", "Szczotka do wymiany", "14/03/2017", "przykladowa tresc4"));
+	}
+
+	public void send() {
+		Utils.addDetailMessage("Wiadomość wysłana", FacesMessage.SEVERITY_INFO);
+	}
 
 	public List<Message> getMessages() {
 		return messages;
@@ -38,5 +48,33 @@ public class MessageView {
 	public void setMessages(List<Message> messages) {
 		this.messages = messages;
 	}
-    
+
+	public void findMsgById(Integer id) {
+
+	}
+
+	public List<Message> getSelectedMsg() {
+		return selectedMsg;
+	}
+
+	public void setSelectedMsg(List<Message> selectedMsg) {
+		this.selectedMsg = selectedMsg;
+	}
+
+	public List<Message> getFilteredValue() {
+		return filteredValue;
+	}
+
+	public void setFilteredValue(List<Message> filteredValue) {
+		this.filteredValue = filteredValue;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
 }
